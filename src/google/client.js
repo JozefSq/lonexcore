5fea078ab01876bd5d92d33e5180dae64e2d55c5
JozefSq <105242243+JozefSq@users.noreply.github.com>
2026-04-12 19:38:41 +0200

    feat: update google/client.js - add listFolder, searchFiles, remove Gmail sendEmail

--- src/google/client.js
+++ src/google/client.js
@@ -8,7 +8,6 @@ const auth = new google.auth.GoogleAuth({
   keyFile: path.resolve(process.env.GOOGLE_CREDENTIALS_PATH),
   scopes: [
     'https://www.googleapis.com/auth/drive',
-    'https://www.googleapis.com/auth/gmail.send',
     'https://www.googleapis.com/auth/documents'
   ]
 });
@@ -19,6 +18,29 @@ async function getDriveFiles() {
   const res = await drive.files.list({
     pageSize: 20,
     fields: 'files(id, name, mimeType, webViewLink, size)'
+  });
+  return res.data.files;
+}
+
+async function listFolder(folderId = 'root') {
+  const client = await auth.getClient();
+  const drive = google.drive({ version: 'v3', auth: client });
+  const res = await drive.files.list({
+    q: `'${folderId}' in parents and trashed = false`,
+    fields: 'files(id, name, mimeType, webViewLink)',
+    pageSize: 25,
+    orderBy: 'folder,name'
+  });
+  return res.data.files;
+}
+
+async function searchFiles(query) {
+  const client = await auth.getClient();
+  const drive = google.drive({ version: 'v3', auth: client });
+  const res = await drive.files.list({
+    q: `name contains '${query}' and trashed = false`,
+    fields: 'files(id, name, mimeType, webViewLink)',
+    pageSize: 10
   });
   return res.data.files;
 }
@@ -46,8 +68,6 @@ async function getDownloadLink(fileId) {
 async function uploadFileFromUrl(url, fileName) {
   const client = await auth.getClient();
   const drive = google.drive({ version: 'v3', auth: client });
-
-  // Stiahni subor do tmp
   const tmpPath = path.join(os.tmpdir(), fileName);
   await new Promise((resolve, reject) => {
     const file = fs.createWriteStream(tmpPath);
@@ -56,48 +76,21 @@ async function uploadFileFromUrl(url, fileName) {
       file.on('finish', () => { file.close(); resolve(); });
     }).on('error', reject);
   });
-
   const res = await drive.files.create({
     requestBody: { name: fileName },
     media: { body: fs.createReadStream(tmpPath) },
     fields: 'id, name, webViewLink'
   });
-
   fs.unlinkSync(tmpPath);
   return res.data;
-}
-
-async function sendEmail(from, to, subject, body) {
-  const client = await auth.getClient();
-  const gmail = google.gmail({ version: 'v1', auth: client });
-
-  const message = [
-    `From: ${from}`,
-    `To: ${to}`,
-    `Subject: ${subject}`,
-    'Content-Type: text/plain; charset=utf-8',
-    'MIME-Version: 1.0',
-    '',
-    body
-  ].join('\r\n');
-
-  const encoded = Buffer.from(message)
-    .toString('base64')
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_')
-    .replace(/=/g, '');
-
-  await gmail.users.messages.send({
-    userId: 'me',
-    requestBody: { raw: encoded }
-  });
 }
 
 module.exports = {
   auth,
   getDriveFiles,
+  listFolder,
+  searchFiles,
   findFileByName,
   getDownloadLink,
-  uploadFileFromUrl,
-  sendEmail
+  uploadFileFromUrl
 };
